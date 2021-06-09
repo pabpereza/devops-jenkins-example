e82ac20eaa1b1b04e1cfc6284b01d16a849a1810
Pablo Pérez-Aradros Calvo <pabloperezaradros@gmail.com>
2021-06-09 20:12:44 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
-import requests
+from flask import Flask
+app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-def curl(url):
-    r = requests.get(url)
-    
-    return r.content
-print("hola");
-print( "Prueba caché docker")
-print( curl("https://www.yahoo.com"))
+if __name__ == '__main__':
+      app.run(host='0.0.0.0', port=80)
